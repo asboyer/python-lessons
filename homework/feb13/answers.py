6a6814a27547c69fd1918c933083ac6ecb0b7ca1
Andrew Boyer <asboyer@gmail.com>
2021-02-20 13:29:09 -0500

    added gitignore and started answers

--- homework/feb13/answers.py
+++ homework/feb13/answers.py
@@ -1,1 +1,17 @@
 # Answers will be here after the next lesson!
+
+#1: 
+# Solution: a computer program is a set of instructions that causes a computer to perform some kind of action
+
+#2:
+# Hardware is what physically makes up a computer, like a hard drive or a circuit board, and software can be written or installed on the hardware and runs on the hardware 
+
+#3:
+print("Andrew")
+
+#4:
+print("\n^  ^\n____\n")
+
+#5:
+print("\n ___\n|[_]|\n|+ ;|\n`---'")
+# NOTE: "\n" <- line break
