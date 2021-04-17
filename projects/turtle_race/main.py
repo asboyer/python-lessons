0e2d75f6f747983c9728628e06f310aca7ea53bd
boyer <asboyer@gmail.com>
2021-04-17 17:29:34 -0400

    added more code

--- projects/turtle_race/main.py
+++ projects/turtle_race/main.py
@@ -1,4 +1,5 @@
 from turtle import *
+from random import randint
 
 # main setup
 speed(0)
@@ -18,9 +19,54 @@ for step in range(16):
     forward(20)
 
 # add the racing turtles
+# eric
 eric = Turtle()
 eric.color('red')
 eric.shape('turtle')
+
+# kevin
+kevin = Turtle()
+kevin.color('blue')
+kevin.shape('turtle')
+
+# carson
+carson = Turtle()
+carson.color('green')
+carson.shape('turtle')
+
+# elijah
+elijah = Turtle()
+elijah.color('yellow')
+elijah.shape('turtle')
+
+# move turtle to starting line
+eric.penup()
+kevin.penup()
+carson.penup()
+elijah.penup()
+eric.goto(-160, 100)
+kevin.goto(-160, 70)
+carson.goto(-160, 40)
+elijah.goto(-160, 10)
+eric.pendown()
+kevin.pendown()
+carson.pendown()
+elijah.pendown()
+
+# totals
+eric_total = 0
+
+# race the turtles
+for turn in range(100):
+    eric_step = randint(1, 5)
+    eric.forward(eric_step)
+    eric_total += eric_step
+    kevin.forward(randint(1, 5))
+    carson.forward(randint(1, 5))
+    elijah.forward(randint(1, 5))    
+
+# interact with user
+guess = input("What turtle will win?")
 
 # keep the turtle window open
 mainloop()
