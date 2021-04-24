2a13595e92a902a16bae28cc358f403444b1e362
boyer <asboyer@gmail.com>
2021-04-24 16:52:16 -0400

    turtle race updates

--- projects/turtle_race/main.py
+++ projects/turtle_race/main.py
@@ -55,18 +55,62 @@ elijah.pendown()
 
 # totals
 eric_total = 0
+kevin_total = 0
+carson_total = 0
+elijah_total = 0
+
+guess = input("Who will win?")
 
 # race the turtles
 for turn in range(100):
+    
     eric_step = randint(1, 5)
     eric.forward(eric_step)
     eric_total += eric_step
-    kevin.forward(randint(1, 5))
-    carson.forward(randint(1, 5))
-    elijah.forward(randint(1, 5))    
 
-# interact with user
-guess = input("What turtle will win?")
+    kevin_step = randint(1, 5)
+    kevin.forward(kevin_step)
+    kevin_total += kevin_step
+
+    carson_step = randint(1, 5)
+    carson.forward(carson_step)
+    carson_total += carson_step
+
+    elijah_step = randint(1, 5)
+    elijah.forward(elijah_step)
+    elijah_total += elijah_step
+
+totals = [eric_total, kevin_total, carson_total, elijah_total]
+
+max_total = 0
+max_index = 0
+for i in range(len(totals)):
+    if totals[i] > max_total:
+        max_total = totals[i]
+        max_index = i
+
+print(f"Eric (Red): {eric_total}")
+print(f"Kevin (Blue): {kevin_total}")
+print(f"Carson (Green): {carson_total}")
+print(f"Elijah (Yellow): {elijah_total}")
+
+if max_index == 0:
+    print('Eric Wins!')
+    winner = 'eric'
+elif max_index == 1:
+    print('Kevin Wins!')
+    winner = 'kevin'
+elif max_index == 2:
+    print('Carson wins!')
+    winner = 'carson'
+else:
+    print('Elijah wins')
+    winner = 'elijah'
+
+if guess.lower().strip().startswith(winner):
+    print("YOU GUESSED THE WINNER!")
+else:
+    print("YOU DID NOT GUESS THE WINNER!")
 
 # keep the turtle window open
 mainloop()
