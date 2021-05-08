1846a13dc03459e82bf996951a79e44404c667b6
boyer <asboyer@gmail.com>
2021-05-08 17:37:20 -0400

    new cypher

---
+++ projects/vigenere/lesson.py
@@ -0,0 +1,21 @@
+
+for i in range(5): # i is a temporary variable 
+    # everything in loop is defined as what is indented
+    print('this is in the loop') ##
+    print(i) # 0 1 2 3 4
+    # i ++ ++
+
+# write a loop that prints all numbers from 1 - 100 (inclusive)
+# in your loop, if a number is divisible by 5, print "fizz" instead of the num
+
+if(10 % 2 == 0):
+    print("10 is divisible by 2 because the remainder is 0.")
+
+
+
+for logan in range(1, 5):
+    # do this
+    # THIS IS IN THE LOOP
+
+# THIS IS OUT OF THE LOOP
+
