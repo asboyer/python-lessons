21723c2ad36884eb0519b8082c9f735bae37afbf
boyer <asboyer@gmail.com>
2021-06-07 17:26:11 -0400

    updating project

--- projects/turtle_race/v3.py
+++ projects/turtle_race/v3.py
@@ -45,17 +45,50 @@ def start_line(turtles, y):
         turtle["turtle"].goto(x, y)
         y -= 30
 
-start_line(turtle_dict, y)
+def print_name(name):
+    return name[0].upper() + name[1:].lower()
 
+start_line(turtle_dict, y)
 
 def race(turtle):
     step = randint(1, 5)
     turtle["turtle"].forward(step)
     turtle["steps"] += step
 
+guess = input("Your winner: ")
+
 for i in range(100):
     for turtle in turtle_dict.values():
         race(turtle)
 
+results = []
+
 for name, turtle in turtle_dict.items():
-    print(f"{name}: {turtle['steps']}")
+    print(f"{name[0].upper() + name[1:]}: {turtle['steps']}")
+    results.append([turtle['steps'], name])
+
+results.sort()
+results.reverse()
+
+maximum = results[0][0]
+winners = []
+
+for result in results:
+    if result[0] == maximum:
+        winners.append(result[1])
+    else:
+        break
+
+if len(winners) > 1:
+    for i in winners:
+        if not winners[i] == winners[len(winners) - 1]:
+            print(print_name(winners[i]), end=" and")
+        else:
+            print(print_name(winners[i]) + " all tie for the win!")
+else:
+    print(print_name(winners[0]) + " wins!")
+
+if guess.lower().strip() in winners:
+    print("You guessed the winner!")
+else:
+    print("You did not guess the winner!")
