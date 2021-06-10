53eea90cb68dad1089e9f4ac1437dda485df54c2
boyer <asboyer@gmail.com>
2021-06-10 17:57:20 -0400

    new lesson

--- projects/adventure/main.py
+++ projects/adventure/main.py
@@ -1,5 +1,6 @@
 from boyer import *
 from art import text2art
+from os import path
 
 # maintenance variables
 speed = 0
@@ -36,7 +37,29 @@ def intro(played):
 
 def choose(level, choices):
     aprint(fr(f'descriptions/description{str(level)}_{choices}'))
-    return get_num(fr(f'choices/choice/{str(level)}_{choices}'), start=0, finish=1, integer=True)
+    return get_num("\n" + fr(f'choices/choice{str(level)}_{choices}'), start=0, finish=1, integer=True)
+
+def check_alive(level, choice):
+    death_file = f"deaths/death{str(level)}_{str(choice)}"
+
+    if path.exists(f'text/{death_file}.txt'):
+        aprint(fr(death_file))
+        return False
+    else:
+        return True
+
+def check_win(level, choice):
+    win_file = f"wins/win{str(level)}_{str(choice)}"
+
+    if path.exists(f"text/{win_file}.txt"):
+        aprint(fr(win_file))
+        return True
+    else:
+        return False
+
+def credits():
+    aprint(fr('credits'))
+
 
 while playing:
 
@@ -47,11 +70,37 @@ while playing:
     alive = True
     win = False
     level = 1
+    score_break = False
+    initials = ""
 
     intro(played)
 
     while alive and not win:
         choice = choose(level, choices)
+        choices += str(choice)
+        speed = 4
+        alive = check_alive(level, choices)
+        win = check_win(level, choices)
+        if alive and not win:
+            score += 2 * level
+            level += 1
+        if score > highscore and not score_break:
+            highscore = score
+            print("new high score!".upper())
+            score_break = True
+            if initials != "":
+                print(f"You broke {initials}'s score!")
 
-        # description1_.txt
-    
+    played+=1
+
+    if win:
+        print(text2art("\nyou win!\n".upper()))
+        score += 1000 * level
+        credits()
+
+    else:
+        print(text2art("\ngame over!\n".upper()))
+
+    if score_break:
+        aprint("You set a new high score!")
+        initials = input("Please enter your initials: ")
