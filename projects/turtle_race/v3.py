2171ff5df613a3cb6ebe6832f097c157cb0e4009
boyer <asboyer@gmail.com>
2021-06-14 17:32:55 -0400

    new lesson

--- projects/turtle_race/v3.py
+++ projects/turtle_race/v3.py
@@ -1,5 +1,16 @@
 from turtle import *
 from random import randint
+from boyer import delay_print
+
+# admin variables
+text_speed = 0
+simulation = True
+
+play = True
+
+intro_file = open("intro.txt")
+intro_text = intro_file.read()
+delay_print(intro_text, text_speed)
 
 # a race track
 startx = -140
@@ -34,7 +45,8 @@ for name, color in blueprint:
     turtle_dict[name]["color"] = color
     turtle_dict[name]["turtle"].color(color)
     turtle_dict[name]["steps"] = 0
-
+    turtle_dict[name]["total_steps"] = 0
+    turtle_dict[name]["wins"] = 0
 
 x = startx - 20
 y = starty - 40
@@ -48,47 +60,94 @@ def start_line(turtles, y):
 def print_name(name):
     return name[0].upper() + name[1:].lower()
 
-start_line(turtle_dict, y)
+def clean(string):
+    return string.lower().strip()
 
 def race(turtle):
     step = randint(1, 5)
     turtle["turtle"].forward(step)
     turtle["steps"] += step
 
-guess = input("Your winner: ")
+def play_again():
+    while True:
+        response = input('\nPlay again? ')
 
-for i in range(100):
-    for turtle in turtle_dict.values():
-        race(turtle)
-
-results = []
-
-for name, turtle in turtle_dict.items():
-    print(f"{name[0].upper() + name[1:]}: {turtle['steps']}")
-    results.append([turtle['steps'], name])
-
-results.sort()
-results.reverse()
-
-maximum = results[0][0]
-winners = []
-
-for result in results:
-    if result[0] == maximum:
-        winners.append(result[1])
-    else:
-        break
-
-if len(winners) > 1:
-    for i in winners:
-        if not winners[i] == winners[len(winners) - 1]:
-            print(print_name(winners[i]), end=" and")
+        if response.lower().strip().startswith('y'):
+            return True
+        elif response.lower().strip().startswith('n'):
+            return False
         else:
-            print(print_name(winners[i]) + " all tie for the win!")
-else:
-    print(print_name(winners[0]) + " wins!")
+            print('Please enter a yes or no!')
+            continue
 
-if guess.lower().strip() in winners:
-    print("You guessed the winner!")
-else:
-    print("You did not guess the winner!")
+def get_winner():
+    while True:
+        guess = input("\nWho will win? ")
+
+        if clean(guess) in names or clean(guess) in colors:
+            return guess
+        else:
+            print('Please give a valid guess!')
+            continue
+
+while play:
+
+    for turtle in turtle_dict.values():
+        turtle['steps'] = 0
+
+    start_line(turtle_dict, y)
+
+
+    
+        guess = get_winner()
+
+    for i in range(100):
+        for turtle in turtle_dict.values():
+            race(turtle)
+
+    results = []
+
+    for name, turtle in turtle_dict.items():
+        print(f"{name[0].upper() + name[1:]}: {turtle['steps']}")
+        print(f"{name[0].upper() + name[1:]}: {turtle['total_steps']}")
+        results.append([turtle['steps'], name])
+
+    results.sort()
+    results.reverse()
+
+    maximum = results[0][0]
+    winners = []
+
+    for result in results:
+        if result[0] == maximum:
+            winners.append(result[1])
+        else:
+            break
+
+    if len(winners) > 1:
+        for i in range(len(winners)):
+            if not winners[i] == winners[len(winners) - 1]:
+                print(print_name(winners[i]), end=" and")
+            else:
+                print(print_name(winners[i]) + " all tie for the win!")
+    else:
+        print(print_name(winners[0]) + " wins!")
+
+    winner_colors = []
+
+    for winner in winners:
+        winner_colors.append(turtle_dict[winner]["color"])
+        turtle_dict[winner]["wins"] += 1
+
+    print("\nTurtle stats:")
+    for name, turtle in turtle_dict.items():
+        print(f'{print_name(name)}: {turtle["wins"]} wins')
+        turtle["total_steps"] += turtle["steps"]
+
+    if guess.lower().strip() in winners or guess.lower().strip() in winner_colors:
+        print("\nYou guessed the winner!")
+    else:
+        print("\nYou did not guess the winner!")
+
+    play = play_again()
+    
