45b8f1318bdba8a180c448000c5b91447140c47b
boyer <asboyer@gmail.com>
2021-07-17 17:31:29 -0400

    new lesson

--- projects/rock_paper/main.py
+++ projects/rock_paper/main.py
@@ -10,7 +10,12 @@ if name == '' or name == ' ':
 # constant variables
 computer_score = 0
 player_score = 0
+
+# TODO: implement streak
+
 choices = ['rock', 'paper', 'scissors', 'scissor', 'r', 'p', 's']
+
+commands = ['clear', 'quit', 'score']
 
 # getting valid input
 while True:
@@ -19,6 +24,17 @@ while True:
 
         if player_choice in choices:
             break
+        elif player_choice in commands:
+            
+            # TODO: write these functions
+            if player_choice == 'clear':
+                clear()
+            if player_choice == 'quit':
+                quit()
+            if player_choice == 'score':
+                score()
+
+            continue
         else:
             print("Please enter a valid input!")
             continue
@@ -47,6 +63,50 @@ while True:
     print("\n" + name + " choice: " + player_choice)
     print("Computer choice: " + computer_choice + "\n")
 
+    player_win = False
+    computer_win = False
+
+#______________________________________________________
+
+    # DRAW
+    if player_choice == computer_choice:
+        pass
+
+    # PLAYER WINS
+    elif player_choice == 'rock' and computer_choice == 'scissors':
+        player_win = True
+    
+    elif player_choice == 'paper' and computer_choice == 'rock':
+        player_win = True
+
+    elif player_choice == 'scissors' and computer_choice == 'paper':
+        player_win = True
+
+    # COMPUTER WINS
+    elif computer_choice == 'rock' and player_choice =='scissors':
+        computer_win = True
+
+    elif computer_choice == 'paper' and player_choice == 'rock':
+        computer_win = True
+
+    elif computer_choice == 'scissors' and player_choice == 'rock':
+        computer_win = True
+
+#__________________________________
+
+
+    if player_win:
+        print(f'{name} wins!\n')
+        player_score += 1
+
+    elif computer_win:
+        print(f'{name} loses!\n')
+        computer_score += 1
+
+    else:
+        print('Draw\n')
+
+# ____________________________________
 
 
 
--- projects/rock_paper/sim.py
+++ projects/rock_paper/sim.py
@@ -26,11 +26,11 @@ for i in range(amount_of_simulations):
             scissors += 1
 
     # fraction = amount of rocks/amount of times played
-    print("______________________________")
-    print("In a sample of 10 rounds:")
-    print("Amount of rock: " + str(rock)) # should be 1/3 
-    print("Amount of paper: " + str(paper)) # should be 1/3
-    print("Amount of scissors: " + str(scissors) + "\n") # should be 1/3
+    # print("______________________________")
+    # print("In a sample of 10 rounds:")
+    # print("Amount of rock: " + str(rock)) # should be 1/3 
+    # print("Amount of paper: " + str(paper)) # should be 1/3
+    # print("Amount of scissors: " + str(scissors) + "\n") # should be 1/3
 
     list_of_outcomes = [rock, paper, scissors]
 
@@ -39,5 +39,6 @@ for i in range(amount_of_simulations):
             if outcome == number:
                 number_of_outcomes[number] += 1
 
+print(f'In {str(amount_of_simulations)} rounds of 10:')
 for number in range(len(number_of_outcomes)):
     print(f'Amount of {str(number)}\'s: {str(number_of_outcomes[number])}') #TODO: print the probability of this outcome occruing
