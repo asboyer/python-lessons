e07624f843ccf8e7aa300498226c7e6d547aa41c
Andrew Boyer <asboyer@gmail.com>
2021-08-12 17:58:45 -0400

    automated commit

--- projects/pygame/ball_template.py
+++ projects/pygame/ball_template.py
@@ -2,13 +2,13 @@ import pygame
 from random import randint
 
 class Ball:
-    def __init__(self):
-        self.radius = 10
-        self.color = WHITE
-        self.x = randint(0, screen_width)
-        self.y = randint(0, screen_width)
-        self.xv = randint(-max_v, max_v)
-        self.yv = randint(-max_v, max_v)
+    def __init__(self, radius, color, x, y, xv, yv):
+        self.radius = radius
+        self.color = color
+        self.x = x
+        self.y = y
+        self.xv = xv
+        self.yv = yv
 
     def move(self):
         if self.x >= screen_width - self.radius or self.x <= 0:
@@ -17,33 +17,57 @@ class Ball:
         if self.y >= screen_width - self.radius or self.y <= 0:
             self.yv = -self.yv
 
+        self.x += self.xv
+        self.y += self.yv
+
         pygame.draw.ellipse(screen, self.color, [self.x, self.y, self.radius, self.radius], 0)
 
+def randcolor():
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
+    return (r, g, b)
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
 
-x = 200
-y = 200
-
-ball_color = WHITE
 background_color = BLACK
 
-xv = 10
-yv = 10
+screen_width = 700
+ball_width = 20
+ball_amount = 300
 
 max_v = 5
+min_radius = 30
+max_radius = 100
+
+ball_list = []
 
 done = False
-
-screen_width = 400
-ball_width = 50
 
 pygame.init()
 
 screen = pygame.display.set_mode((screen_width, screen_width))
 pygame.display.set_caption('Bouncing Ball')
 clock = pygame.time.Clock()
+
+for i in range(ball_amount):
+    ball_width = randint(min_radius, max_radius)
+    ball_color = randcolor()
+    ball_x = randint(0, screen_width - ball_width)
+    ball_y = randint(0, screen_width - ball_width)
+    ball_xv = randint(-max_v, max_v)
+    ball_yv = randint(-max_v, max_v)
+    ball = Ball(
+                ball_width,
+                ball_color, 
+                ball_x,
+                ball_y,
+                ball_xv,
+                ball_yv
+                )
+    ball_list.append(ball)
 
 while not done:
     for event in pygame.event.get():
@@ -52,16 +76,8 @@ while not done:
 
     screen.fill(background_color)
 
-    if x >= screen_width - ball_width or x <= 0:
-        xv = -xv
-
-    if y >= screen_width - ball_width or y <= 0:
-        yv = -yv
-
-    pygame.draw.ellipse(screen, ball_color, [x, y, ball_width, ball_width], 0)
-   
-    x += xv
-    y += yv
+    for ball in ball_list:
+        ball.move()
 
     pygame.display.update()
     clock.tick(60)
