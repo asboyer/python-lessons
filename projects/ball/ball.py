eb2b72855bf13b697a71a8119e3a65a534bead86
Andrew Boyer <asboyer@gmail.com>
2021-10-04 17:55:03 -0400

    ball project edits

--- projects/ball/ball.py
+++ projects/ball/ball.py
@@ -1,6 +1,7 @@
 import pygame
 from random import randint
 from random import choice
+import time
 
 # a class is something that stores variables and functions in
 # objects 
@@ -19,83 +20,93 @@ class Ball:
         self.path = path
         self.rm = rm
         self.points = []
+        self.wall_hits = 0
 
+        print(xv)
+        print(yv)
         if self.oxv < 0:
             self.oxv = -self.oxv
         
         if self.x < screen_width/2 and self.y < screen_width/2:
             self.q = 2
-            self.color = (255, 255, 255)
 
         elif self.x < screen_width/2 and self.y > screen_width/2:
             self.q = 3
-            self.color =  (0, 255, 0)
 
         elif self.x > screen_width/2 and self.y > screen_width/2:
             self.q = 4
-            self.color = (0, 0, 255)
 
         elif self.x > screen_width/2 and self.y < screen_width/2:
             self.q = 1
-            self.color = (255, 0, 0)
     
     def move(self):        
         #________________________________________________
         if self.x < screen_width/2 and self.y < screen_width/2:
             self.q = 2
-            self.color = (255, 255, 255)
 
         elif self.x < screen_width/2 and self.y > screen_width/2:
             self.q = 3
-            self.color =  (0, 255, 0)
 
         elif self.x > screen_width/2 and self.y > screen_width/2:
             self.q = 4
-            self.color = (0, 0, 255)
 
         elif self.x > screen_width/2 and self.y < screen_width/2:
             self.q = 1
-            self.color = (255, 0, 0)
         # _____________________________________________________
 
-        if self.x >= screen_width - self.radius:
-            print(f'xv before: {self.xv}')
-            self.xv = -self.xv
-            low = int(self.xv/2)
-            high = int(-self.xv/2)
-            if low > high:
-                low = -low
-                high = -high
-            variance = randint(low, high)
-            print(f'variance: {variance}')
-            self.xv += variance
-            if self.x + self.xv >= screen_width - self.radius:
-                self.xv = -self.oxv
-            if self.xv > max_v:
-                self.xv = self.oxv
-            print(f'xv after: {self.xv}')
-            print("______________________")
+        if self.rm:    
+            if self.x >= screen_width - self.radius:
+                self.wall_hits += 1
+                print(f'xv before: {self.xv}')
+                self.color = randcolor()    
+                self.xv = -self.xv
+                low = int(self.xv/2)
+                high = int(-self.xv/2)
+                if low > high:
+                    low = -low
+                    high = -high
+                variance = randint(low, high)
+                print(f'variance: {variance}')
+                self.xv += variance
+                if self.x + self.xv >= screen_width - self.radius:
+                    self.xv = -self.oxv
+                if self.xv > max_v:
+                    self.xv = self.oxv
+                print(f'xv after: {self.xv}')
+                print("______________________")
 
-        if self.x <= 0:
-            print(f'xv before: {self.xv}')
-            self.xv = -self.xv
-            low = int(-self.xv/2)
-            high = int(self.xv/2)
-            if low > high:
-                low = -low
-                high = -high
-            variance = randint(low, high)  
-            print(f'variance: {variance}')
-            self.xv += variance
-            if self.x + self.xv <= 0:
-                self.xv = self.oxv
-            if self.xv > max_v:
-                self.xv = self.oxv
-            print(f'xv after: {self.xv}')
-            print("______________________")
+            if self.x <= 0:
+                print(f'xv before: {self.xv}')
+                self.wall_hits += 1
+
+                self.color = randcolor()    
+                self.xv = -self.xv
+                low = int(-self.xv/2)
+                high = int(self.xv/2)
+                if low > high:
+                    low = -low
+                    high = -high
+                variance = randint(low, high)  
+                print(f'variance: {variance}')
+                self.xv += variance
+                if self.x + self.xv <= 0:
+                    self.xv = self.oxv
+                if self.xv > max_v:
+                    self.xv = self.oxv
+                print(f'xv after: {self.xv}')
+                print("______________________")
+        else:
+            if self.x >= screen_width - self.radius or self.x <= 0:
+                self.xv = - self.xv
+                self.wall_hits += 1
+
+                self.color = randcolor()    
 
         if self.y >= screen_width - self.radius or self.y <= 0:
             self.yv = - self.yv
+            self.wall_hits += 1
+
+            self.color = randcolor()
 
         self.points.append([self.x, self.y, self.color])
         self.x += self.xv
@@ -113,6 +124,8 @@ class Ball:
                                     int(self.radius/4),
                                     int(self.radius/4)], 
                                     0)
+    def get_wh(self):
+        return self.wall_hits
 
 # color format "rgb"
 # (0, 0, 0)
@@ -132,9 +145,9 @@ RED = (255, 0, 0)
 bg_color = BLACK
 screen_width = 1400
 
-ball_amount = 1
+ball_amount = 3
 
-max_v = 100
+max_v = 20
 min_radius = 50
 max_radius = 200
 
@@ -147,7 +160,7 @@ screen = pygame.display.set_mode((screen_width, screen_width))
 pygame.display.set_caption('Ball')
 clock = pygame.time.Clock()
 
-for i in range(ball_amount):
+def randball():
     ball_width = randint(min_radius, max_radius)
     ball_color = randcolor()
     ball_x = randint(ball_width, screen_width - ball_width)
@@ -155,7 +168,7 @@ for i in range(ball_amount):
 
     ball_xv, ball_yv = 0, 0
 
-    while ball_xv == 0 and ball_yv == 0:
+    while (ball_xv == 0 or ball_yv == 0) or (ball_xv == ball_yv):
         ball_xv = randint(-max_v, max_v)
         ball_yv = randint(-max_v, max_v)
 
@@ -165,18 +178,31 @@ for i in range(ball_amount):
                 ball_x,
                 ball_y,
                 ball_xv,
-                ball_yv
+                ball_yv,
+                True,
+                True
                 )
+    return ball  
 
-    ball_list.append(ball)
+def balls(ball_list):
+    for i in range(ball_amount):
+        ball = randball()
+        ball_list.append(ball)
+    return ball_list
+
+
+ball_list = balls(ball_list)
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
     screen.fill(bg_color)
-    for ball in ball_list:
-        ball.move()
+    for i in range(len(ball_list)):
+        ball_list[i].move()
+    for i in range(len(ball_list)):
+        if ball_list[i].get_wh() > 10:
+            ball_list = balls([])
     pygame.display.update()
     clock.tick(60)
 
